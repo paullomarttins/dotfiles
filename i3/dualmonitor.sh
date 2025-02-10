8640cd40103965f602288bc5b3126eb62d56afb3
Paulo Martins <p.o.marttins@gmail.com>
2025-02-09 21:39:17 -0300

    Configuração dual monitor

--- i3/dualmonitor.sh
+++ i3/dualmonitor.sh
@@ -2,16 +2,16 @@
 interno=eDP-1
 externo=HDMI-1
 
-if xrandr | grep "$externo disconnected"; then
-	xrandr --output "$externo" --off --output "$interno" --primary  --auto
+if xrandr | grep $externo disconnected; then
+	xrandr --output $externo --off --output $interno --primary  --auto
 else
-	xrandr --output "$interno" --primary --auto --output $externo --right-of "$interno" --auto
+	xrandr --output $interno --primary --auto --output $externo --right-of $interno --auto
 fi
 
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload example &
-  done
-else
-  polybar --reload example &
-fi
+#if type "xrandr"; then
+#  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+#    MONITOR=$m polybar --reload example &
+#  done
+#else
+#  polybar --reload example &
+#fi
