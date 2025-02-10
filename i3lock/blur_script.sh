a08b8e925626b4b51ab5b2d8e56e37f0dcdbb30b
Paulo Martins <p.o.marttins@gmail.com>
2025-02-09 21:45:05 -0300

    Novo script para 3lock blur

--- i3lock/blur_script.sh
+++ i3lock/blur_script.sh
@@ -1,10 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 TMPBG=/tmp/screen.png
-LOCK=~/.config/i3lock/lock.png
+#LOCK=~/.config/i3lock/lock.png
+SCREENSHOT="scrot -z $TMPBG"
 
-import -window root $TMPBG
-convert $TMPBG -channel RGBA -blur 0x8 $TMPBG \
-&& composite -gravity center $LOCK $TMPBG $TMPBG
+BLUR="5x4"
 
+$SCREENSHOT 
+#convert -scale 10% -blur 0x0.5 -resize 1000% $TMPBG $TMPBG
+convert $TMPBG -blur $BLUR $TMPBG
+#magick composite -gravity center $LOCK $TMPBG $TMPBG
+#pkill i3lock
 i3lock -i $TMPBG
 rm $TMPBG
