8de1e426263b9880ebf900d12fe99b7ab1d345a2
Paulo Martins <p.o.marttins@gmail.com>
2025-12-20 15:03:41 -0300

    atualização lock.png

--- .config/i3lock/blur_script.sh
+++ .config/i3lock/blur_script.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 TMPBG=/tmp/screen.png
-#LOCK=~/.config/i3lock/lock.png
+LOCK=~/.config/i3lock/lock.png
 SCREENSHOT="scrot -z $TMPBG"
 
 BLUR="5x4"
@@ -9,7 +9,7 @@ BLUR="5x4"
 $SCREENSHOT 
 #convert -scale 10% -blur 0x0.5 -resize 1000% $TMPBG $TMPBG
 convert $TMPBG -blur $BLUR $TMPBG
-#magick composite -gravity center $LOCK $TMPBG $TMPBG
+magick composite -gravity center $LOCK $TMPBG $TMPBG
 #pkill i3lock
 i3lock -i $TMPBG
 rm $TMPBG
